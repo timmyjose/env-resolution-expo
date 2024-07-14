09f7e6c147ce4ee49aae193a93668965d9f98051
Timmy Jose <zoltan.jose@gmail.com>
2024-07-14 11:57:32 +0530

    fix: fixed incorrect sentry dsn.

--- src/components/SentryDemo.tsx
+++ src/components/SentryDemo.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, TextInput } from 'react-native'
+import { View, StyleSheet, Button, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
@@ -8,12 +8,14 @@ import * as Sentry from '@sentry/react-native'
 const SentryDemo = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamsList>>()
 
-  const [errMsg, setErrMsg] = useState<string>('default error message')
+  const [errMsg, setErrMsg] = useState<string>('')
 
   return (
     <View style={styles.container}>
       <Button title='Go Back' onPress={() => navigation.goBack()} />
       <TextInput
+        style={styles.input}
+        placeholder='Enter error message'
         value={errMsg}
         onChangeText={text => setErrMsg(text)}
       />
@@ -28,6 +30,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  input: {
+    margin: 5,
+    paddingLeft: 2,
+    borderColor: 'orange',
+    borderWidth: 1,
+    height: 40,
+    width: '70%'
   }
 })
 
